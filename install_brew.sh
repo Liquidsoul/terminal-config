35bc682270f47fc69913c9320d14c12423d44e1a
Liquidsoul <liquidsoul@liquidsoul.fr>
2015-10-17 23:11:31 +0200

    [cleanup] factorize install_brew code into functions

--- install_brew.sh
+++ install_brew.sh
@@ -2,23 +2,37 @@
 
 # Install `brew` and `brew bundle`
 
-if ! which -s brew > /dev/null; then
-  echo "brew not found, installing it..."
-  # install command from http://brew.sh
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  echo "brew installed, continuing..."
-fi
+function install_brew() {
+  if ! which -s brew > /dev/null; then
+    echo "brew not found, installing it..."
+    # install command from http://brew.sh
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    echo "brew installed, continuing..."
+  fi
+}
 
-if brew tap-info homebrew/bundle | grep 'Not installed'; then
-  echo "brew bundle not found, installing it..."
-  brew tap Homebrew/bundle
-fi
+function install_brew_bundle() {
+  if brew tap-info homebrew/bundle | grep 'Not installed'; then
+    echo "brew bundle not found, installing it..."
+    brew tap Homebrew/bundle
+  else
+    echo "brew bundle installed, continuing..."
+  fi
+}
 
-SCRIPT_DIR="$( cd -P "$( dirname "$0" )" && pwd )"
-pushd $SCRIPT_DIR > /dev/null
+function execute_brew_bundle() {
+  SCRIPT_DIR="$( cd -P "$( dirname "$0" )" && pwd )"
+  pushd $SCRIPT_DIR > /dev/null
 
-echo "Processing brewfile..."
-brew bundle
+  echo "Processing brewfile..."
+  brew bundle
 
-popd > /dev/null
+  popd > /dev/null
+}
+
+install_brew
+
+install_brew_bundle
+
+execute_brew_bundle
